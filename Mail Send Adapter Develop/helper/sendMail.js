1c996c2f8debe7a43f1c54f7f30d0a406bb704d3
mamun1646 <mimbd01130004@gmail.com>
2022-09-21 20:22:32 +0600

    Bulk mail and Verification File Added

---
+++ Mail Send Adapter Develop/helper/sendMail.js
@@ -0,0 +1,69 @@
+const nodemailer=require('nodemailer');
+exports.sendMail = async (email,otp) => {
+    try{
+        const MailingPurpose={
+            varifyEmail:"Please verify your email with the given OTP"
+        }
+        let mailTransporter=nodemailer.createTransport({
+            service: "gmail",
+            auth:{
+                user:"testabcd134@gmail.com",
+                pass:"Nopassword@#$%"
+            }
+        })
+        
+        let details={
+            from:"testabcd134@gmail.com",
+            to:`${email}`,
+            subject:"Email Varification",
+            text:"",
+            html:`<br><br> <p>Dear User,</p><br/> <p>${MailingPurpose.varifyEmail}<br/><br/>OTP is : ${otp}</p>`
+        }
+        
+       await mailTransporter.sendMail(details,(err)=>{
+            if(err){
+                console.log("Sending Error ",err);
+            }
+            else{
+                console.log("Mail Sent Perfectly");
+            }
+        })
+    } catch (e){
+        console.log("Error Occured");
+    }
+    
+  };
+
+  exports.sendUserMail = async (email,subject,body,footer) => {
+    try{
+        let mailTransporter=nodemailer.createTransport({
+            service: "gmail",
+            auth:{
+                user:"testabcd134@gmail.com",
+                pass:"Nopassword@#$%"
+            }
+        })
+        
+        let details={
+            from:"testabcd134@gmail.com",
+            to:`${email}`,
+            subject:`${subject}`,
+            text:"",
+            html:`<br><br><p>${body} </p><br/><br/><p>${footer}</p>`
+        }
+        
+       await mailTransporter.sendMail(details,(err)=>{
+            if(err){
+                console.log("Sending Error ",err);
+            }
+            else{
+                console.log("Mail Sent Perfectly");
+            }
+        })
+    } catch (e){
+        console.log("Error Occured");
+    }
+    
+  };
+
+
